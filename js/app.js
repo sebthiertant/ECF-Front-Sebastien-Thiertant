ef23e52021fdd16cb2b43348ae9c8fa49a2bdb62
BeTr4y4l <75473312+BeTr4y4l@users.noreply.github.com>
2021-05-23 14:48:15 +0200

    addind more html pages and continuing to work on the site

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
-// const invalid = document.querySelector('.input:invalid');
-// console.log(invalid)
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     const input = Array.from(document.querySelectorAll('.input'));
+//     const input_name = document.querySelector('.input-name');
+//     const input_mail = document.querySelector('.input-mail');
+//     const input_message = document.querySelector('.input-message');
+//     for (let i of input) {
+//         i.setAttribute('required', true);
+
+//     }
+
+//     document.querySelector('form').addEventListener('submit', (e) => {
+//             e.preventDefault();
+//         })
+//         if (input_name.validity.valid) {
+//             console.log('good')
+//         }
+//         for (let i of input) {
+//             i.setCustomValidity("Ce champ est requis");
+//             i.reportValidity();
+//         }
+
+// });
