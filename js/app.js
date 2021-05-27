c2c5a776a7f3852ad8a5316bac527835b998be65
BeTr4y4l <75473312+BeTr4y4l@users.noreply.github.com>
2021-05-27 23:10:29 +0200

    working on burger menu form ok

--- js/app.js
+++ js/app.js
@@ -1,42 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // navbar
+    const toggleMenu = document.querySelector('.main-nav__toggler');
+    const menu = document.querySelector('.menu-list');
 
-    const form = document.querySelector('.form')
-    const button = document.querySelector('.button-form')
-
-
-    // function name_validity() {
-    //     const input_name = document.querySelector('.input_name')
-
-    //     console.log(input_name.checkValidity())
-    //     if (input_name.checkValidity() === true) {
-    //         console.log("ok")
-    //         input_name.setCustomValidity("")
-    //     } else {
-    //         console.log("erreur")
-    //         input_name.setCustomValidity("Ce champ est requis")
-    //     }
-    // }
-
-
-
-    // document.querySelector('.input_name').setCustomValidity("Ce champ est requis")
-
-    // if (document.querySelector('.input_name').value != "") {
-    //     document.querySelector('.input_name').setCustomValidity("")
-    // }
-
-    // button.addEventListener('click', name_validity)
-
-    // navbar 
-
-    // const toggleMenu = document.querySelector('.main-nav__toggler');
-    // const menu = document.querySelector('.main-nav-list');
-
-    // toggleMenu.addEventListener('click', function() {
-    //     const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
-    //     toggleMenu.setAttribute('aria-expanded', !open);
-    //     menu.hidden = !menu.hidden;
-    // })
-
-
+    toggleMenu.addEventListener('click', function() {
+        const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
+        toggleMenu.setAttribute('aria-expanded', !open);
+        menu.hidden = !menu.hidden;
+    })
 });
